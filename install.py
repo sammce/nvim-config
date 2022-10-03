badb171781595fe67d9fe28e07db5805edc647e7
Sam McElligott <sammcelligott@outlook.com>
2022-10-03 22:04:26 +0100

    Finalise MacOS install script

--- install.py
+++ install.py
@@ -1,45 +1,69 @@
 #!/usr/bin/env python3
+
 import os
 import platform
 import subprocess
 import sys
-from pprint import pprint
 
 from formatting import Formatter as fmt
 
 
-class test_subprocess:
-    @staticmethod
-    def run(cmd):
-        print(fmt.bold("Running command: "))
-        print(fmt.underline(" ".join(cmd)))
+class SafeSubprocess:
+    __safe_commands = ["node", "brew", "git"]
+
+    def __init__(self, is_test: bool):
+        self.is_test = is_test
+
+    def run(self, cmd):
+        if isinstance(cmd, list):
+            print("$", fmt.underline(" ".join(cmd)))
+        else:
+            print("$", fmt.underline(cmd))
+
+        # Only run command if not testing, or command is safe
+        if (not self.is_test) or (cmd[0] in self.__safe_commands):
+            return subprocess.run(cmd)
+
+
+def error(msg: str):
+    print(fmt.error(msg))
+    sys.exit(1)
+
+
+def info(msg: str):
+    print()
+    print(fmt.info(fmt.bold(msg)))
+
+
+def success(msg: str):
+    print(fmt.success(fmt.bold(msg)))
 
 
 class Test:
-    def __init__(self, os: str):
-        self.test = len(sys.argv) > 1 and "--test" in sys.argv[1:]
+    def __init__(self, user_os: str):
+        self.is_test = len(sys.argv) > 1 and "--test" in sys.argv[1:]
 
-        if self.test:
-            print(fmt.bold(fmt.info("Running in TEST mode")))
-            print("-" * 40)
+        if self.is_test:
+            info("Running in TEST mode")
 
-            subprocess.run = test_subprocess.run
             self.paths = {
                 "config_file": "./tests/.config/nvim/init.vim",
                 "config_folder": "./tests/.config/nvim",
                 "plugin_folder": "./tests/plug.vim",
-                "node_folder": "./tests/local/bin/node",
+                "node_folder": ["./tests/local/bin/node"],
                 "brew_folder": "./tests/homebrew/bin/brew",
                 "sh_rc": "./tests/.bashrc" if os == "linux" else "./tests/.zshrc",
             }
         else:
             self.paths = {
-                "config_file": "~/.config/nvim/init.vim",
-                "config_folder": "~/.config/nvim",
-                "plugin_folder": '"${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim',
+                "config_file": f"{os.environ['HOME']}/.config/nvim/init.vim",
+                "config_folder": f"{os.environ['HOME']}/.config/nvim",
+                "plugin_folder": f"{os.environ['HOME']}/.local/share/nvim/site/autoload/plug.vim",
                 "node_folder": "/usr/local/bin/node",
                 "brew_folder": "/opt/homebrew/bin/brew",
-                "sh_rc": "~/.bashrc" if os == "linux" else "~/.zshrc",
+                "sh_rc": f"{os.environ['HOME']}/.bashrc"
+                if user_os == "linux"
+                else f"{os.environ['HOME']}/.zshrc",
             }
 
     def run_tests(self):
@@ -52,17 +76,13 @@ class Test:
         pass
 
 
-def error(msg: str):
-    print(fmt.error(msg))
-    sys.exit(1)
-
-
 class Commands:
-    def __init__(self, is_root: bool, test: Test):
+    def __init__(self, is_dcu: bool, test: Test):
         # Collect info about the system
-        self.is_root = is_root
+        self.is_dcu = is_dcu
         self.test = test
         self.cwd = os.getcwd()
+        self.subprocess = SafeSubprocess(test.is_test)
 
     def install_nvim(self):
         raise NotImplementedError("nvim installation not implemented")
@@ -71,50 +91,74 @@ class Commands:
         raise NotImplementedError("ctags installation not implemented")
 
     def add_aliases(self):
+        info("Adding aliases to your shell rc file...")
+        if not os.path.exists(self.test.paths["sh_rc"]):
+            self.subprocess.run(["touch", self.test.paths["sh_rc"]])
+            success(f"Created shell rc file at {self.test.paths['sh_rc']}")
+
         with open(self.test.paths["sh_rc"], "a") as f:
             f.write("\n# Nvim aliases\n")
             f.write(f"alias nvim='{self.cwd}/nvim'\n")
             f.write(f"alias nv='{self.cwd}/nvim'\n")
 
-    def copy_config(self):
-        print(fmt.bold("Copying nvim config..."))
-        if not os.path.exists(self.test.paths["config_folder"]):
-            subprocess.run(["mkdir", "-p", self.paths["config_folder"]])
+        success("Added aliases")
 
-        filename = "sudo_init.vim" if self.is_root else "init.vim"
+    def copy_config(self):
+        info("Copying nvim config...")
+        if not os.path.exists(self.test.paths["config_folder"]):
+            self.subprocess.run(["mkdir", "-p", self.test.paths["config_folder"]])
 
         if not os.path.exists(self.test.paths["config_file"]):
-            subprocess.run(
-                ["cp", f"./scripts/{filename}", self.test.paths["config_file"]]
+            self.subprocess.run(
+                ["cp", f"./scripts/init.vim", self.test.paths["config_file"]]
             )
         else:
             with open(self.test.paths["config_file"], "a") as f:
-                with open(f"./scripts/{filename}", "r") as f2:
+                with open(f"./scripts/init.vim", "r") as f2:
                     f.write(f2.read())
 
+        success("Copied nvim config")
+
     def install_vim_plug(self):
-        print(fmt.bold("Installing vim-plug..."))
-        subprocess.run(
+        info("Installing vim-plug...")
+        self.subprocess.run(
             [
-                "sh",
-                "-c",
-                f"'curl -fLo {self.test.paths['plugin_folder']} --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'",
+                "curl",
+                "-fLo",
+                self.test.paths["plugin_folder"],
+                "--create-dirs",
+                "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
             ]
         )
 
+        success("Installed vim-plug")
+
     def check_node(self):
-        print(fmt.bold("Checking for node installation..."))
-        if not os.path.exists(self.test.paths["node_folder"]):
+        info("Checking for node installation...")
+        node = self.subprocess.run(["node", "-v"])
+        if not node:
             error("Node is not installed. Please install node before continuing.")
 
+        success("Node is installed")
+
     def check_git(self):
-        print(fmt.bold("Checking for git installation..."))
-        if not os.path.exists("/usr/bin/git"):
+        info("Checking for git installation...")
+        git = self.subprocess.run(["git", "--version"])
+        if not git:
             error("Git is not installed. Please install git before continuing.")
 
+        success("Git is installed")
+
     def install_plugins(self):
-        print(fmt.bold("Installing nvim plugins..."))
-        subprocess.run(["nvim", "--headless", "+PlugInstall", "+qall"])
+        info("Installing nvim plugins...")
+        self.subprocess.run(["nvim", "--headless", "+PlugInstall", "+qall"])
+
+        if not self.is_dcu:
+            self.subprocess.run(
+                ["nvim", "--headless", "+CocInstall coc-pyright", "+qall"]
+            )
+
+        success("Installed nvim plugins")
 
     def install(self):
         # Determine OS -
@@ -126,7 +170,7 @@ class Commands:
         # Install ctags -
         # Add aliases if not root -
         # Install plugins -
-        print(fmt.bold("Starting installation..."))
+        info("Starting installation...")
         self.check_node()
         self.check_git()
 
@@ -134,51 +178,67 @@ class Commands:
         self.copy_config()
         self.install_vim_plug()
 
-        if self.is_root:
-            self.install_ctags()
+        if self.is_dcu:
             self.add_aliases()
+        else:
+            self.install_ctags()
 
         self.install_plugins()
+
+        with open(self.test.paths["config_file"], "a") as f:
+            f.write("\n:colorscheme onehalfdark")
 
 
 class MacCommands(Commands):
     def install_nvim(self):
-        print(fmt.bold("Installing nvim..."))
-        subprocess.run("brew install neovim")
+        info("Installing nvim...")
+        self.subprocess.run(["brew", "install", "neovim"])
+        success("Installed nvim")
 
     def install_ctags(self):
-        print(fmt.bold("Installing ctags..."))
-        subprocess.run(["xcode-select", "--install"])
-        subprocess.run(["brew", "tap", "universal-ctags/universal-ctags"])
-        subprocess.run(["brew", "install", "--HEAD", "universal-ctags"])
+        info("Installing ctags...")
+        self.subprocess.run(["xcode-select", "--install"])
+        self.subprocess.run(["brew", "tap", "universal-ctags/universal-ctags"])
+        self.subprocess.run(["brew", "install", "--HEAD", "universal-ctags"])
+        success("Installed ctags")
+
+    def check_brew(self):
+        info("Checking for brew installation...")
+
+        brew = self.subprocess.run(["brew", "-v"])
+
+        if not brew:
+            error("Brew is not installed. Please install brew before continuing.")
+
+        success("Brew is installed")
 
 
 class LinuxCommands(Commands):
     def install_nvim(self):
-        print(fmt.bold("Installing nvim..."))
-        if self.is_root:
-            subprocess.run("sudo apt install neovim")
+        info("Installing nvim...")
+        if not self.is_dcu:
+            self.subprocess.run("sudo apt install neovim")
         else:
-            subprocess.run(
+            self.subprocess.run(
                 [
                     "curl",
                     "-LO",
                     "https://github.com/neovim/neovim/releases/latest/download/nvim.appimage",
                 ]
             )
-            subprocess.run(["chmod", "u+x", "nvim.appimage"])
-            subprocess.run(["mv", "nvim.appimage", "nvim"])
-            subprocess.run(["cd", ".."])
+            self.subprocess.run(["chmod", "u+x", "nvim.appimage"])
+            self.subprocess.run(["mv", "nvim.appimage", "nvim"])
 
-            with open("~/.bashrc", "a") as f:
-                f.write(f"alias nvim='{os.getcwd()}/nvim'")
+        success("Installed nvim")
 
     def install_ctags(self):
-        print(fmt.bold("Installing ctags..."))
-        subprocess.run(["git", "clone", "https://github.com/universal-ctags/ctags.git"])
-        subprocess.run(["cd", "ctags"])
+        info("Installing ctags...")
+        self.subprocess.run(
+            ["git", "clone", "https://github.com/universal-ctags/ctags.git"]
+        )
+        self.subprocess.run(["cd", "ctags"])
 
-        subprocess.run(
+        self.subprocess.run(
             [
                 "sudo",
                 "apt",
@@ -196,16 +256,19 @@ class LinuxCommands(Commands):
             ]
         )
 
-        subprocess.run(["./autogen.sh"])
-        subprocess.run(["./configure"])
-        subprocess.run(["make"])
-        subprocess.run(["make", "install"])
-        subprocess.run(["cd", ".."])
+        self.subprocess.run(["./autogen.sh"])
+        self.subprocess.run(["./configure"])
+        self.subprocess.run(["make"])
+        self.subprocess.run(["make", "install"])
+        self.subprocess.run(["cd", ".."])
+
+        success("Installed ctags")
 
 
 def main():
-    is_root = os.geteuid() == 0
     user_os = platform.system()
+
+    test = Test(user_os)
 
     # Kill the script if the OS is not supported
     if user_os == "Windows":
@@ -213,31 +276,17 @@ def main():
             "Windows is not supported. Please use WSL or switch to Linux and run this script again."
         )
 
-    # Check if user meant to run without sudo, only if not on dcu lab computer.
-    if (not is_root) and platform.node() != "soc-razor1":
-        print(
-            fmt.error(
-                "You are not root. Some plugins (such as autocomplete) won't be installed. You can run as root (sudo) to fix this."
-            )
-        )
-        continue_install = input(fmt.bold("Continue without sudo? [y/N]: ")).lower()
-        if continue_install != "y":
-            print(fmt.info("Exiting..."))
-            sys.exit(0)
-
-    test = Test(user_os)
+    is_dcu = len(sys.argv) > 1 and "--dcu" in sys.argv[1:]
 
     if user_os == "Darwin":
-        if not os.path.exists(test.paths["brew_folder"]):
-            error("Homebrew is not installed. Install it and then rerun this script.")
-
-        commands = MacCommands(is_root, test)
+        commands = MacCommands(is_dcu, test)
+        commands.check_brew()
     else:
-        commands = LinuxCommands(is_root, test)
+        commands = LinuxCommands(is_dcu, test)
 
     commands.install()
 
-    if test.test:
+    if test.is_test:
         test.run_tests()
 
 
---
+++ scripts/init-base.vim
@@ -0,0 +1,32 @@
+" Nvim related settings
+:set number
+:set relativenumber
+:set autoindent
+:set tabstop=4
+:set shiftwidth=4
+:set smarttab
+:set softtabstop=4
+:set mouse=a
+:set encoding=UTF-8
+
+" Install plugins
+call plug#begin()
+
+Plug 'https://github.com/vim-airline/vim-airline' " Status bar
+Plug 'https://github.com/preservim/nerdtree' " Tree-like file explorer
+Plug 'https://github.com/tpope/vim-surround' " Surrounding ysw)
+Plug 'https://github.com/tpope/vim-commentary' " For commenting gcc & gc
+Plug 'https://github.com/ap/vim-css-color' " CSS color preview
+Plug 'https://github.com/rafi/awesome-vim-colorschemes' " Retro scheme(s)
+Plug 'https://github.com/ryanoasis/vim-devicons' " Developer icons
+Plug 'https://github.com/terryma/vim-multiple-cursors' " CTRL + N for multiple cursors
+Plug 'https://github.com/jiangmiao/auto-pairs' " Auto closes brackets, quotes etc
+
+call plug#end()
+
+let g:NERDTreeDirArrowExpandable="+"
+let g:NERDTreeDirArrowCollapsible="-"
+
+nnoremap <C-f> :NERDTreeFocus<CR>
+nnoremap <C-n> :NERDTreeFocus<CR>
+nnoremap <C-t> :NERDTreeToggle<CR>
